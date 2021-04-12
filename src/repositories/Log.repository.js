1ad5429c395d587734b1c184912ec60b236ff0c9
Janno Tabamo <janno@hyperstacksinc.com>
2021-04-12 14:48:12 +0800

    add database

--- src/repositories/Log.repository.js
+++ src/repositories/Log.repository.js
@@ -1,23 +1,23 @@
-// const { LogModel } = require("../models");
+const { LogModel } = require("../models");
 
 const create = async (log) => {
-  return true; // await LogModel.create(log);
+  return await LogModel.create(log);
 };
 
 const findAll = async () => {
-  return true; // await LogModel.findAll();
+  return await LogModel.findAll();
 };
 
 const findOne = async () => {
-  return true; // await LogModel.findOne();
+  return await LogModel.findOne();
 };
 
 const update = async (id, data) => {
-  return true; // await LogModel.update(data, { where: { id } });
+  return await LogModel.update(data, { where: { id } });
 };
 
 const remove = async (id) => {
-  return true; // await LogModel.destroy({ where: { id } });
+  return await LogModel.destroy({ where: { id } });
 };
 
 module.exports = {
